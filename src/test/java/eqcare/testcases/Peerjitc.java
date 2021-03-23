db2d97e50e1cbad64d5ed2a14b483c501ec03905
sarikadhall <sarikadhall@localhost>
2021-03-23 15:32:56 -0400

    peerji - pom changes

---
+++ src/test/java/eqcare/testcases/Peerjitc.java
@@ -0,0 +1,42 @@
+package eqcare.testcases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import NotRequiredClasses.ContactUsPage;
+import eqcare.pages.HomePage;
+import eqcare.pages.LoginPage;
+import eqcare.pages.LogoutPage;
+import eqcare.pages.RegistrationPage;
+import eqcare.factories.DataProviderFactory;
+import eqcare.pages.BaseClass;
+
+public class Peerjitc extends BaseClass{
+	
+	//  2. Patient login to the application to EQ Care
+
+	LoginPage login;
+	HomePage home;
+
+	@Test(priority = 0)
+	public void verifyPage() throws Exception {
+
+		login = PageFactory.initElements(driver, LoginPage.class);
+
+		home = PageFactory.initElements(driver, HomePage.class);
+
+		logger = report.createTest("Pony Web Application Login URL validation");
+
+		login.verifyUrlBeforeLogin();
+
+		logger.info("Validating url");
+		
+		//login.peerji();
+		
+		//login.runTestResults("29754173");
+
+	}
+
+	
+
+}
