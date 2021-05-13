e86c3d7c68e22d487191d44b30f27f0d2bb921ab
sarikadhall <sarikadhall@localhost>
2021-05-13 08:40:29 -0400

    profile pic

--- src/test/java/eqcare/peerjitestcases/TC2_RegisterationScenariosAV.java
+++ src/test/java/eqcare/peerjitestcases/TC2_RegisterationScenariosAV.java
@@ -88,7 +88,7 @@ public class TC2_RegisterationScenariosAV extends BaseClass {
 				DataProviderFactory.getExcel().getCellData("Users", 2, 8),
 
 				DataProviderFactory.getExcel().getCellData("Users", 2, 9),
-				DataProviderFactory.getExcel().getCellData("Users", 2, 10), false);
+				DataProviderFactory.getExcel().getCellData("Users", 2, 10), true);
 
 		register.registerationCompleteWithValidInput();
 
