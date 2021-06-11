abc3e8a76f819bc3a9fa599e8bb4357ca5383204
sarikadhall <sarikadhall@192.168.1.6>
2021-06-11 12:48:27 -0400

    1797

---
+++ src/test/java/eqcare/peerjitestcases/copy/Novapeerji1797.java
@@ -0,0 +1,59 @@
+package eqcare.peerjitestcases.copy;
+
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import nova.pages.LoginPage;
+import nova.pages.NovaHomePage;
+import eqcare.factories.DataProviderFactory;
+import eqcare.pages.copy.BaseClass;
+
+public class Novapeerji1797 extends BaseClass {
+
+	LoginPage login;
+	NovaHomePage home;
+	
+	//test
+
+	@Test(priority = 0)
+	public void verifyPage() throws Exception {
+
+		login = PageFactory.initElements(driver, LoginPage.class);
+
+		login.verifyUrlBeforeLogin();
+
+	}
+	
+	@Parameters({"NovaEmail","NovaPassword"})
+	@Test(priority = 1, dependsOnMethods = "verifyPage")
+	public void loginToApplication(String novaemail, String novapassword) throws Exception {
+		
+		login.loginToApplication(novaemail,
+				novapassword);
+		
+		login.verifyUrlAfterLogin();
+
+	}
+	
+	@Parameters({"SearchEmployer","CoverageToken","CoverageType"})
+	@Test(priority = 2, dependsOnMethods = "loginToApplication", enabled=false)
+	public void CreateEmployer(String searchemployer, String coveragetoken, String coveragetype ) throws InterruptedException {
+		
+		home = PageFactory.initElements(driver, NovaHomePage.class);
+
+		home.createEmployerNew1797("Sarika-Test-PC-LJ","70", "primary_care_with_lifejourney"); // First see if employer there Modify
+		
+		home.createCoverage1797("Sarika-Test-PC-LJ","70", "primary_care_with_lifejourney");
+		
+		home.createService1797("Sarika-Test-PC-LJ","70", "primary_care_with_lifejourney");
+		
+	}
+
+	
+	
+	
+	
+	
+}
