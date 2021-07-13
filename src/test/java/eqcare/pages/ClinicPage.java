fd9504a26f32ab3d1ee9d1bde42dc2f510b79213
sarikadhall <sarikadhall@192.168.1.6>
2021-07-13 15:11:14 -0400

    clinic

--- src/test/java/eqcare/pages/ClinicPage.java
+++ src/test/java/eqcare/pages/ClinicPage.java
@@ -31,8 +31,11 @@ public class ClinicPage {
 	
 	By commentsLink=By.xpath("//a[contains(text(), 'Comments')]");
 	
+	By commentsTextArea=By.xpath("//textarea[contains(@aria-label, 'Comment')]");
 	
+	By commentsToggle=By.xpath("//input[contains(@aria-label, 'Share')]");
 	
+	By commentsButton=By.xpath("//div[contains(text(), 'Comment')]");
 	
 	public void loginToApplication(String userName,String passWord)
 	{
@@ -61,6 +64,13 @@ public class ClinicPage {
 	public void addComments() {
 
 		Utility.waitForWebElement(driver, commentsLink).click();
+		
+		Utility.waitForWebElement(driver, commentsTextArea).sendKeys("Test");
+		
+		Utility.waitForWebElement(driver, commentsToggle).click();
+		
+		Utility.waitForWebElement(driver, commentsButton).click();
+		
 
 	}
 
