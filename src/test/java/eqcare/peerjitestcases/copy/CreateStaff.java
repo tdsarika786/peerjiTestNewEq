0794995f94750ed99999ffe5d98c74d9d1b41432
sarikadhall <sarikadhall@192.168.1.6>
2021-07-15 19:43:17 -0400

    create staff

---
+++ src/test/java/eqcare/peerjitestcases/copy/CreateStaff.java
@@ -0,0 +1,67 @@
+package eqcare.peerjitestcases.copy;
+
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import nova.pages.LoginPage;
+import nova.pages.NovaHomePage;
+import eqcare.factories.DataProviderFactory;
+import eqcare.pages.copy.BaseClass;
+
+public class CreateStaff extends BaseClass {
+
+	LoginPage login;
+	NovaHomePage home;
+	
+	//testpeerji
+
+	@Test(priority = 0)
+	public void verifyPage() throws Exception {
+
+		login = PageFactory.initElements(driver, LoginPage.class);
+
+		login.verifyUrlBeforeLogin();
+
+	}
+	
+	@Parameters({"NovaEmail","NovaPassword"})
+	@Test(priority = 1, dependsOnMethods = "verifyPage")
+	public void loginToApplication(String novaemail, String novapassword) throws Exception {
+		
+		login.loginToApplication(novaemail,
+				novapassword);
+		
+		login.verifyUrlAfterLogin();
+
+	}
+	
+	
+	@Test(priority = 2, dependsOnMethods = "loginToApplication")
+	public void CreateStaff() throws InterruptedException {
+		
+		home = PageFactory.initElements(driver, NovaHomePage.class);
+
+        home.addStaff("saru@eqcare.com", "secret", "Nurse");
+	}
+	
+	@Test(priority = 3, dependsOnMethods = "loginToApplication")
+	public void CreateStaff1() throws InterruptedException {
+		
+		home = PageFactory.initElements(driver, NovaHomePage.class);
+
+        home.addStaff("anku@eqcare.com", "secret", "CareManager");
+	}
+	
+	@Test(priority = 4, dependsOnMethods = "loginToApplication")
+	public void CreateStaff2() throws InterruptedException {
+		
+		home = PageFactory.initElements(driver, NovaHomePage.class);
+
+        home.addStaff("anu@eqcare.com", "secret", "Practitioner");
+	}
+	
+	
+	
+}
