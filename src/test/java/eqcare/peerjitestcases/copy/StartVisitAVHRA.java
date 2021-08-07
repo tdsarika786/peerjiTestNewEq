4077806798f4196998aed1e2641b9c002853c2db
sarikadhall <sarikadhall@192.168.1.6>
2021-08-06 22:28:36 -0400

    hra visists

---
+++ src/test/java/eqcare/peerjitestcases/copy/StartVisitAVHRA.java
@@ -0,0 +1,75 @@
+package eqcare.peerjitestcases.copy;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import eqcare.helper.Utility;
+import eqcare.pages.ClinicPage;
+import eqcare.pages.HomePage;
+import eqcare.pages.HomePageAV;
+import eqcare.pages.LifeJourneyIntakeFormPage;
+import eqcare.pages.IntakeFormPage;
+import eqcare.pages.IntakeFormPageAV;
+import eqcare.pages.LoginPageAV;
+import eqcare.pages.copy.BaseClass;
+
+public class StartVisitAVHRA extends BaseClass {
+
+	ClinicPage clinic, clinic1, clinic2, clinic3;
+	LoginPageAV login;
+	IntakeFormPageAV intake;
+	HomePageAV home;
+
+	@Parameters({ "AVAppURL" })
+	@Test(priority = 0)
+	public void loginToApplication(String AVAppURL) throws Exception {
+
+		login = PageFactory.initElements(driver, LoginPageAV.class);
+
+		login.AccessVirtualURL(AVAppURL);
+
+		login.loginToApplication("sarikaqa7+emp70@gmail.com", "Sarika#123");
+
+		login.verifyUrlAfterLogin(); // peerji
+
+		System.out.println("*************START VISIT ******************** sarikaqa7+av1@gmail.com ");
+
+	}
+
+	@Test(priority = 1, dependsOnMethods = "loginToApplication")
+	public void startVideoConsultation() throws Exception {
+
+		home = PageFactory.initElements(driver, HomePageAV.class);
+
+		home.navigateToHomePage();
+
+		System.out.println("************* 1-start Video ConsultationT ********************");
+
+	}
+
+	@Test(priority = 2, dependsOnMethods = "loginToApplication")
+	public void deviceCheck() throws Exception {
+
+		home = PageFactory.initElements(driver, HomePageAV.class);
+
+		home.deviceCheck();
+
+		System.out.println("************* 2-device Check ********************");
+
+	}
+
+	@Test(priority = 3, dependsOnMethods = "deviceCheck")
+	public void fillIntakeFormWithAllFields() throws Exception {
+
+		intake = PageFactory.initElements(driver, IntakeFormPageAV.class);
+
+		intake.fillIntakeForm();
+
+		System.out.println("************* 3-device Check ********************");
+
+		System.out.println("*************END ********************");
+
+	}
+
+}
---
+++ src/test/java/eqcare/peerjitestcases/copy/StartVisitHRARoomImpScore.java
@@ -0,0 +1,74 @@
+package eqcare.peerjitestcases.copy;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import eqcare.helper.Utility;
+import eqcare.pages.ClinicPage;
+import eqcare.pages.HomePage;
+import eqcare.pages.HomePageAV;
+import eqcare.pages.LifeJourneyIntakeFormPage;
+import eqcare.pages.IntakeFormPage;
+import eqcare.pages.IntakeFormPageAV;
+import eqcare.pages.LoginPage;
+import eqcare.pages.copy.BaseClass;
+
+public class StartVisitHRARoomImpScore extends BaseClass {
+
+	ClinicPage clinic, clinic1, clinic2, clinic3;
+	LoginPage login;
+	IntakeFormPage intake;
+	HomePage home;
+
+	// Patient Start Visit
+	@Parameters({ "Email", "Password" })
+	@Test(priority = 0)
+	public void loginToApplication(String email, String password) throws Exception {
+
+		login = PageFactory.initElements(driver, LoginPage.class);
+
+		login.loginToApplication("sarikaqa7+emp73@gmail.com", "Sarika#123");
+
+		login.verifyUrlAfterLogin();
+		
+		System.out.println("*************START VISIT ******************** "+email);
+
+	}
+
+	@Test(priority = 1, dependsOnMethods = "loginToApplication")
+	public void startVideoConsultation() throws Exception {
+
+		home = PageFactory.initElements(driver, HomePage.class);
+
+		home.navigateToHomePage();
+		
+		System.out.println("************* 1-start Video ConsultationT ********************");
+
+	}
+
+	@Test(priority = 2, dependsOnMethods = "loginToApplication")
+	public void deviceCheck() throws Exception {
+
+		home = PageFactory.initElements(driver, HomePage.class);
+		
+		home.hraCompleteLater();
+
+		home.deviceCheck();
+		
+		System.out.println("************* 2-device Check ********************");
+
+	}
+
+	@Test(priority = 3, dependsOnMethods = "deviceCheck")
+	public void fillIntakeFormWithAllFields() throws Exception {
+
+		intake = PageFactory.initElements(driver, IntakeFormPage.class);
+
+		intake.fillIntakeForm();
+		
+		System.out.println("************* 3-device Check ********************");
+
+		System.out.println("*************END ********************");
+	}
+}
