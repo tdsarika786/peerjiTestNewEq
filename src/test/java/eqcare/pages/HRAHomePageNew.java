1c2cd7e5d8229981642ec9e076256ffc1a87ec73
sarikadhall <sarikadhall@localhost>
2021-08-13 15:31:07 -0400

    minutes

--- src/test/java/eqcare/pages/HRAHomePageNew.java
+++ src/test/java/eqcare/pages/HRAHomePageNew.java
@@ -61,7 +61,7 @@ public class HRAHomePageNew extends runWithAttachments {
 
 	By introPageHeader = By.xpath("//h3[contains(text(), 'health score assessment')]");
 
-	By introPageMinsText = By.xpath("//p[contains(text(), 'mins')]");
+	By introPageMinsText = By.xpath("//p[contains(text(), 'minutes')]");
 
 	By startHRAFormButton = By.xpath("//a[@data-role='start-hra-form-button']");
 
