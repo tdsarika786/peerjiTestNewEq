dd79b987dcca15c47fd4b27cd798b8aad2ea6096
sarikadhall <sdhall@eqcare.com>
2021-09-14 11:41:56 -0400

    nova links

--- src/test/java/eqcare/peerjitestcases/copy/NovapeerjiAllLinks.java
+++ src/test/java/eqcare/peerjitestcases/copy/NovapeerjiAllLinks.java
@@ -51,71 +51,129 @@ public class NovapeerjiAllLinks extends BaseClass {
 		String empID = "700";
 
 		home.novaPatients(searchemployer1, empID, null);
+
+		System.out.println("********* 1- NOVA novaPatients *************************");
 		
 		home.novaFamilies(searchemployer1, empID, null);
 		
+		System.out.println("********* 2- NOVA novaFamilies *************************");
+		
 		home.novaAppointments(searchemployer1, empID, null);
+		
+		System.out.println("********* 3- NOVA novaAppointments *************************");
 		
 		home.novaVisits(searchemployer1, empID, null);
 		
+		System.out.println("********* 4- NOVA novaVisits *************************");
+		
 		home.novaPaywall(searchemployer1, empID, null);
+		
+		System.out.println("********* 5- NOVA novaPaywall *************************");
 		
 		home.novaNativeApp(searchemployer1, empID, null);
 		
+		System.out.println("********* 6- NOVA novaNativeApp *************************");
+		
 		home.novaMedication(searchemployer1, empID, null);
+		
+		System.out.println("********* 7- NOVA novaMedication *************************");
 		
 		home.novaICD(searchemployer1, empID, null);
 		
+		System.out.println("********* 8- NOVA novaICD *************************");
+		
 		home.novaHRAForms(searchemployer1, empID, null);
+		
+		System.out.println("********* 9- NOVA novaHRAForms *************************");
 		
 		home.novaHRAArticles(searchemployer1, empID, null);
 		
+		System.out.println("********* 10- NOVA novaHRAArticles *************************");
+		
 		home.novaFAQCate(searchemployer1, empID, null);
+		
+		System.out.println("********* 11- NOVA novaFAQCate *************************");
 		
 		home.novaFAQ(searchemployer1, empID, null);
 		
-		home.novaCovidReq(searchemployer1, empID, null);	
+		System.out.println("********* 12- NOVA novaFAQ *************************");
+		
+		home.novaCovidReq(searchemployer1, empID, null);
+		
+		System.out.println("********* 13- NOVA novaCovidReq *************************");
 		
 		home.novaCannedRes(searchemployer1, empID, null);
+		
+		System.out.println("********* 14- NOVA novaCannedRes *************************");
 				
 		home.novaSupportTickets(searchemployer1, empID, null);
 		
+		System.out.println("********* 15- NOVA novaSupportTickets *************************");
+		
 		home.novaStaff(searchemployer1, empID, null);
+		
+		System.out.println("********* 16- NOVA novaStaff *************************");
 		
 		home.novaRatings(searchemployer1, empID, null);
 		
+		System.out.println("********* 17- NOVA novaRatings *************************");
+		
 		home.novaClinicLoggedActions(searchemployer1, empID, null);
+		
+		System.out.println("********* 18- NOVA novaClinicLoggedActions *************************");
 		
 		home.novaIncidentReports(searchemployer1, empID, null);
 		
+		System.out.println("********* 19- NOVA novaIncidentReports *************************");
+		
 		home.novaClinics(searchemployer1, empID, null);
+		
+		System.out.println("********* 20- NOVA novaClinics *************************");
 		
 		home.novaAnnouncements(searchemployer1, empID, null);
 		
+		System.out.println("********* 21- NOVA novaAnnouncements *************************");
+		
 		home.novaPartnerPortals(searchemployer1, empID, null);
+		
+		System.out.println("********* 22- NOVA novaPartnerPortals *************************");
 		
 		home.novaGroupNos(searchemployer1, empID, null);
 		
+		System.out.println("********* 23- NOVA novaGroupNos *************************");
+		
 		home.novaEmployer(searchemployer1, empID, null);
 		
-
+		System.out.println("********* 24- NOVA novaEmployer *************************");
+		
 		home.novaEmployees(searchemployer1, empID, null);
+		
+		System.out.println("********* 25- NOVA novaEmployees *************************");
 		
 		home.novaClientAdmin(searchemployer1, empID, null);
 		
+		System.out.println("********* 26- NOVA novaClientAdmin *************************");
 		
 		home.novaCarriers(searchemployer1, empID, null);
 		
+		System.out.println("********* 27- NOVA novaCarriers *************************");
+		
 		home.novaAdvisors(searchemployer1, empID, null);
+		
+		System.out.println("********* 28- NOVA novaAdvisors *************************");
 		
 		home.novaStaffKPI(searchemployer1, empID, null);
 		
+		System.out.println("********* 29- NOVA novaStaffKPI *************************");
+		
 		home.novaClientKPI(searchemployer1, empID, null);
+		
+		System.out.println("********* 30- NOVA novaClientKPI *************************");
 		
 		home.novaClinicKPILink(searchemployer1, empID, null);
 		
-
-		System.out.println("********* NOVA Patients Table ***********");
+		System.out.println("********* 31- NOVA novaClinicKPILink *************************");
+		
 	}
 
 	
